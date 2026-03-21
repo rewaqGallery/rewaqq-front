13e78de398b18f1503adf286a34c8c1b86ef4269
mohamed <mohamedelafandy1@gamail.com>
2026-03-21 21:34:03 +0200

    f3

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://localhost:9001";
-const BASE_URL = "https://rewaq-server-production.up.railway.app/";
+const BASE_URL = "https://rewaq-server-production.up.railway.app";
 
 export const getToken = () => localStorage.getItem("token");
 
